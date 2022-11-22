7d1ea73ade2534da5e28521b0734cee285960838
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-11-22 14:52:44 +0900

    data追加機能作成

--- second/diary/views.py
+++ second/diary/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import DayCreateForm
 
 
 def index(request):
   return render(request, 'diary/day_list.html')
+
+
+def add(request):
+  form = DayCreateForm(request.POST or None)
+  if request.method == 'POST' and form.is_valid():
+    form.save()
+    return redirect('diary:index')
+  context = {
+    'form': form
+  }
+
+  return render(request, 'diary/day_form.html', context)
