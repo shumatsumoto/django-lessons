e40bb27f52186d738e440aed952423b5e3a65d8e
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-12-10 21:20:46 +0900

    社員管理システム：リスト表示

--- third/employee/views.py
+++ third/employee/views.py
@@ -1,5 +1,5 @@
 from django.views import generic
+from .models import Employee
 
-
-class IndexView(generic.TemplateView):
-  template_name = "employee/employee_list.html"
+class IndexView(generic.ListView):
+    model=Employee
