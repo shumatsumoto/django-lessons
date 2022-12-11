80b67f3e79fdb2bc09e7e6b83a7b4de244e42f66
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-12-12 00:06:48 +0900

    社員管理システム：検索Form作成

--- third/employee/views.py
+++ third/employee/views.py
@@ -1,5 +1,27 @@
 from django.views import generic
+from .forms import SearchForm
 from .models import Employee
 
 class IndexView(generic.ListView):
     model=Employee
+
+    def get_context_data(self):
+        """テンプレートへ渡す辞書の作成"""
+        context = super().get_context_data()
+        context['form'] = SearchForm(self.request.GET)  
+        return context
+
+    def get_queryset(self):
+        """テンプレートへ渡す「employee_list」を作成する"""
+        form = SearchForm(self.request.GET)
+        form.is_valid() 
+        queryset = super().get_queryset()
+
+        department = form.cleaned_data['department']
+        if department:
+            queryset = queryset.filter(department=department)
+
+        club = form.cleaned_data['club']
+        if club:
+            queryset = queryset.filter(club=club)
+        return queryset
