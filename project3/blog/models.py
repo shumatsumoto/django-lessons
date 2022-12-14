584a5679b26f1b2b0e61289b406f187d790a841f
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-12-14 15:30:18 +0900

    model Post作成

--- project3/blog/models.py
+++ project3/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+  title = models.CharField('title', max_length=255)
+  text = models.TextField('本文')
+  created_at = models.DateTimeField('作成日', default=timezone.now)
