da7b76cf3d79c22eac264a3be0af562be1f554e8
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-12-14 16:10:21 +0900

    Post 一覧表示

--- project3/blog/models.py
+++ project3/blog/models.py
@@ -2,7 +2,16 @@ from django.db import models
 from django.utils import timezone
 
 
+class Category(models.Model):
+  name = models.CharField('カテゴリ名', max_length=255)
+  def __str__(self):
+    return self.name
+
+
 class Post(models.Model):
   title = models.CharField('title', max_length=255)
   text = models.TextField('本文')
   created_at = models.DateTimeField('作成日', default=timezone.now)
+  category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
+  def __str__(self):
+    return self.title
