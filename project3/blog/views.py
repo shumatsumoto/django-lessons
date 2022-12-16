89f3fddbcc7300fb67235192526825cae05e0d76
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-12-16 22:21:05 +0900

    検索処理

--- project3/blog/views.py
+++ project3/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views import generic
 from .models import Post
 
@@ -5,4 +6,8 @@ from .models import Post
 class IndexView(generic.ListView):
     model = Post
     def get_queryset(self):
-        return Post.objects.order_by('-created_at')
+        queryset = Post.objects.order_by('-created_at')
+        keyword = self.request.GET.get('keyword')
+        if keyword:
+            queryset = queryset.filter(Q(title__icontains=keyword) | Q(text__icontains=keyword))
+        return queryset
